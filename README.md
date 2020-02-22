9e847da28863ba148ba009d6b854d8a703134f57
shadow578 <christophnienhaus2@gmail.com>
2020-02-22 13:18:19 +0100

    add project files

---
+++ README.md
@@ -0,0 +1,6 @@
+# No, I'm not installing VLC
+No, I'm not installing VLC is a simple (yet effective) App 
+that catches all ACTION_VIEW intents for the VLC Video player and redirects them to the
+default video player app (or shows a selection dialog if no default is set).
+
+That's all...
---
+++ app/src/main/java/de/shadow578/noimnotinstallingvlc/MainActivity.java
@@ -0,0 +1,16 @@
+package de.shadow578.noimnotinstallingvlc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
